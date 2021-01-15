129c4dd7d86d1272d9396e5e3b81461230de695f
Seungmin Oh <dev.rhcsky@gmail.com>
2021-01-15 10:52:23 +0900

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,85 @@
+import os
+
+import torch
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+
+import Augmentor
+
+
+def get_dataloader(args):
+    train_dir = './data/train_loader'
+    test_dir = './data/test_loader'
+
+    if os.path.exists(train_dir) and os.path.exists(test_dir):
+        print("Data already exist.")
+        train_loader = torch.load(train_dir)
+        test_loader = torch.load(test_dir)
+        return train_loader, test_loader
+
+    normalize = transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2761))
+
+    aug = Augmentor.Pipeline()
+    aug.rotate(0.5, 15, 15)
+    aug.random_distortion(0.5, 6, 6, 10)
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    transform_aug = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        aug.torch_transform(),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    train_dataset = datasets.CIFAR100('./data', train=True, download=True, transform=transform_train)
+    aug_train_dataset = datasets.CIFAR100('./data', train=True, download=False, transform=transform_aug)
+
+    images = []
+    labels = []
+    for x, y in tqdm(train_dataset, desc="concat dataset"):
+        images.append(x)
+        labels.append(y)
+    for x, y in tqdm(aug_train_dataset, desc="concat dataset"):
+        images.append(x)
+        labels.append(y)
+
+    train_loader = DataLoader(CustomDataset(train_dataset, aug_train_dataset), batch_size=args.batch_size,
+                              shuffle=True, num_workers=args.workers, pin_memory=True)
+    test_loader = DataLoader(datasets.CIFAR100('./data', train=False, transform=transform_test),
+                             batch_size=args.batch_size, shuffle=True, num_workers=args.workers, pin_memory=True)
+
+    torch.save(train_loader, train_dir)
+    torch.save(test_loader, test_dir)
+    print("Save train loader and test loader in './data/'")
+
+    return train_loader, test_loader
+
+
+class CustomDataset(Dataset):
+    def __init__(self, images, labels):
+        self.images = images
+        self.labels = labels
+
+        self.length = len(self.images)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        image = self.images[index]
+        label = self.labels[index]
+        return image, label
