82c5f0aae1e99d29263bbf3ceb7251f534917c10
rhcsky <dev.rhcsky@gmail.com>
2021-01-20 17:52:57 +0900

    Add f.close() when file open

--- test.py
+++ test.py
@@ -13,14 +13,14 @@ from resnet import ResNet
 from dataloader import get_dataloader
 from utils import AverageMeter, accuracy
 
-parser = argparse.ArgumentParser(description='Cutmix PyTorch CIFAR-100 Test')
-parser.add_argument('--model_path', default='./runs/*', type=str, metavar='PATH')
-parser.add_argument('--batch_size', default=64)
-parser.add_argument('--workers', default=0)
+parser = argparse.ArgumentParser(description="Cutmix PyTorch CIFAR-100 Test")
+parser.add_argument("--model_path", default="./runs/*", type=str, metavar="PATH")
+parser.add_argument("--batch_size", default=64)
+parser.add_argument("--workers", default=0)
 
 best_err1 = 100
 best_err5 = 100
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def main():
@@ -35,8 +35,8 @@ def main():
 
     print(f"|{'Model name':^20}|{'Top1 err':^15}|{'Top5 err':^15}|{'Top1 acc':^15}|")
     for path in model_path:
-        pth_file = path + '/model_best.pth'
-        conf_file = path + '/params.json'
+        pth_file = path + "/model_best.pth"
+        conf_file = path + "/params.json"
 
         params = json.load(open(conf_file))
         args.__dict__.update(params)
@@ -44,12 +44,12 @@ def main():
         checkpoint = torch.load(pth_file)
 
         model = ResNet(args.depth, 100, args.bottleneck).to(device)
-        model.load_state_dict(checkpoint['model_state_dict'])
+        model.load_state_dict(checkpoint["model_state_dict"])
 
         # evaluate on validation set
         err1, err5, val_loss = validate(val_loader, model, criterion, path)
 
-        print(f'|{os.path.basename(path):^20}|{err1:^15}|{err5:^15}|{100 - err1:^15}|')
+        print(f"|{os.path.basename(path):^20}|{err1:^15}|{err5:^15}|{100 - err1:^15}|")
 
 
 def validate(val_loader, model, criterion, path):
@@ -57,7 +57,7 @@ def validate(val_loader, model, criterion, path):
     top1 = AverageMeter()
     top5 = AverageMeter()
 
-    f = open(f'{path + "/res"}.txt', 'w')
+    f = open(f'{path + "/res"}.txt', "w")
 
     # switch to evaluate mode
     model.eval()
@@ -72,15 +72,16 @@ def validate(val_loader, model, criterion, path):
         (err1, err5), pred_list = accuracy(output.data, target, topk=(1, 5))
 
         for pred in pred_list:
-            f.write(str(pred.item()) + '\n')
+            f.write(str(pred.item()) + "\n")
 
         losses.update(loss.item(), input.size(0))
 
         top1.update(err1.item(), input.size(0))
         top5.update(err5.item(), input.size(0))
 
+    f.close()
     return top1.avg, top5.avg, losses.avg
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
