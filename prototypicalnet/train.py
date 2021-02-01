e902d3a0f7fe9e53f2c50ebebbe1d9c2cf0a08a2
rhcsky <dev.rhcsky@gmail.com>
2021-02-01 11:35:08 +0900

    Update train.py

--- prototypicalnet/train.py
+++ prototypicalnet/train.py
@@ -13,12 +13,13 @@ from prototypical_loss import PrototypicalLoss
 from utils import AverageMeter
 
 best_acc1 = 0
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
+args = get_config()
+writer = SummaryWriter(args.log_dir)
 
 
 def main():
     global args, best_acc1, device
-    args = get_config()
 
     # Init seed
     torch.cuda.cudnn_enabled = False
@@ -38,29 +39,37 @@ def main():
     cudnn.benchmark = True
 
     if args.resume:
-        checkpoint = torch.load(sorted(glob(f'{args.log_dir}/checkpoint_*.pth'), key=len)[-1])
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        start_epoch = checkpoint['epoch']
-        best_acc1 = checkpoint['best_acc1']
+        checkpoint = torch.load(
+            sorted(glob(f"{args.log_dir}/checkpoint_*.pth"), key=len)[-1]
+        )
+        model.load_state_dict(checkpoint["model_state_dict"])
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        start_epoch = checkpoint["epoch"]
+        best_acc1 = checkpoint["best_acc1"]
         # scheduler = OneCyclePolicy(optimizer, args.lr, (args.epochs - start_epoch)*args.iterations)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer,
-                                                    gamma=args.lr_scheduler_gamma,
-                                                    step_size=args.lr_scheduler_step)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer=optimizer,
+            gamma=args.lr_scheduler_gamma,
+            step_size=args.lr_scheduler_step,
+        )
         print(f"load checkpoint {args.exp_name}")
     else:
         start_epoch = 0
         # scheduler = OneCyclePolicy(optimizer, args.lr, args.epochs*args.iterations)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer,
-                                                    gamma=args.lr_scheduler_gamma,
-                                                    step_size=args.lr_scheduler_step)
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optimizer=optimizer,
+            gamma=args.lr_scheduler_gamma,
+            step_size=args.lr_scheduler_step,
+        )
 
-    print(f"model parameter : {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
+    print(
+        f"model parameter : {sum(p.numel() for p in model.parameters() if p.requires_grad)}"
+    )
 
     for epoch in range(start_epoch, args.epochs):
 
-        train_loss = train(train_loader, model, optimizer, criterion)
-        val_loss, acc1 = validate(val_loader, model, criterion)
+        train_loss = train(train_loader, model, optimizer, criterion, epoch)
+        val_loss, acc1 = validate(val_loader, model, criterion, epoch)
 
         if acc1 >= best_acc1:
             is_best = True
@@ -68,27 +77,29 @@ def main():
         else:
             is_best = False
 
-        save_checkpoint({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'best_acc1': best_acc1,
-            'optimizer_state_dict': optimizer.state_dict(),
-        }, is_best)
+        save_checkpoint(
+            {
+                "epoch": epoch,
+                "model_state_dict": model.state_dict(),
+                "best_acc1": best_acc1,
+                "optimizer_state_dict": optimizer.state_dict(),
+            },
+            is_best,
+        )
 
-        writer.add_scalar("Loss/Train", train_loss, epoch)
-        writer.add_scalar("Loss/Val", val_loss, epoch)
-        writer.add_scalar("Acc/val_Top1", acc1, epoch)
-
-        print(f"[{epoch}/{args.epochs}] {train_loss:.3f}, {val_loss:.3f}, {acc1:.3f}, # {best_acc1:.3f}")
+        print(
+            f"[{epoch}/{args.epochs}] {train_loss:.3f}, {val_loss:.3f}, {acc1:.3f}, # {best_acc1:.3f}"
+        )
 
         scheduler.step()
 
     writer.close()
 
 
-def train(train_loader, model, optimizer, criterion):
+def train(train_loader, model, optimizer, criterion, epoch):
     losses = AverageMeter()
     num_support = args.num_support_tr
+    total_epoch = len(train_loader) * epoch
 
     # switch to train mode
     model.train()
@@ -96,7 +107,7 @@ def train(train_loader, model, optimizer, criterion):
         input, target = data[0].to(device), data[1].to(device)
 
         output = model(input)
-        loss, _ = criterion(output, target, num_support)
+        loss, acc1 = criterion(output, target, num_support)
 
         losses.update(loss.item(), input.size(0))
 
@@ -105,14 +116,18 @@ def train(train_loader, model, optimizer, criterion):
         loss.backward()
         optimizer.step()
 
+        writer.add_scalar("Loss/Train", loss.item(), total_epoch + i)
+        writer.add_scalar("Acc/Train", acc1.item(), total_epoch + i)
+
     return losses.avg
 
 
 @torch.no_grad()
-def validate(val_loader, model, criterion):
+def validate(val_loader, model, criterion, epoch):
     losses = AverageMeter()
     top1 = AverageMeter()
     num_support = args.num_support_val
+    total_epoch = len(val_loader) * epoch
 
     # switch to evaluate mode
     model.eval()
@@ -124,6 +139,9 @@ def validate(val_loader, model, criterion):
 
         losses.update(loss.item(), input.size(0))
         top1.update(acc1.item(), input.size(0))
+
+        writer.add_scalar("Loss/Val", loss.item(), total_epoch + i)
+        writer.add_scalar("Acc/Val", acc1.item(), total_epoch + i)
 
     return losses.avg, top1.avg
 
@@ -142,5 +160,5 @@ def save_checkpoint(state, is_best):
         torch.save(state, filename)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
