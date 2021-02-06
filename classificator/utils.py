15d4008e262582ff6acd5f5caec057cfc054c3c2
rhcsky <mangusn1@gmail.com>
2021-02-07 02:40:09 +0900

    Add function to adjust weight decay respectively

--- classificator/utils.py
+++ classificator/utils.py
@@ -30,3 +30,15 @@ def accuracy(output, target, topk=(1,)):
         res.append(wrong_k.mul_(100.0 / batch_size))
 
     return res, pred[:1].squeeze(0)
+
+
+def optional_weight_decay_param(net, l2_value, skip_list=()):
+    decay, no_decay = [], []
+    for name, param in net.named_parameters():
+        if not param.requires_grad:
+            continue  # frozen weights
+        if len(param.shape) == 1 or name.endswith(".bias") or name in skip_list:
+            no_decay.append(param)
+        else:
+            decay.append(param)
+    return [{'params': no_decay, 'weight_decay': 0.}, {'params': decay, 'weight_decay': l2_value}]
