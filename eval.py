284ab9ebe09e037e3570332ddbe1d8363160c9f9
Seungmin Oh <dev.rhcsky@gmail.com>
2021-03-04 11:25:59 +0900

    Revert "Rename arguments from configuration"
    This reverts commit 674902a20f5251cc28094cba048a4845a713ff54.

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from glob import glob
+import json
 
 import torch
 import torch.nn.parallel
@@ -59,7 +60,6 @@ def main():
 
         print(f"|{os.path.basename(path):^20}|{err1:^15}|{err5:^15}|{100 - err1:^15}|")
 
-
 def validate(val_loader, model, criterion, path):
     losses = AverageMeter()
     top1 = AverageMeter()
@@ -89,7 +89,6 @@ def validate(val_loader, model, criterion, path):
 
     f.close()
     return top1.avg, top5.avg, losses.avg
-
 
 if __name__ == "__main__":
     main()
