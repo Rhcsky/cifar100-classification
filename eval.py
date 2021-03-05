77c7bfa7a7ce861e4937b17873d23f0e6d87f409
Seungmin Oh <dev.rhcsky@gmail.com>
2021-03-05 17:07:07 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -14,7 +14,7 @@ from model.wide_resnet import get_wide_resnet
 from model.pyramidnet import PyramidNet
 from bsconv.replacers import BSConvS_Replacer
 from dataloader import get_dataloader
-from utils import accuracy, margin_of_error
+from utils import accuracy, AverageMeter
 
 parser = argparse.ArgumentParser(description="Cutmix PyTorch CIFAR-100 Test")
 parser.add_argument("--model_path", default="./runs/*", type=str, metavar="PATH")
@@ -24,7 +24,7 @@ parser.add_argument("--workers", default=0)
 best_err1 = 100
 best_err5 = 100
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
+margin = 12
 
 def main():
     global args, best_err1, best_err5
@@ -35,16 +35,14 @@ def main():
     criterion = torch.nn.CrossEntropyLoss().to(device)
 
     model_path = glob(args.model_path)
-    max_len_exp = max([len(x) for x in model_path]) + 2
+    max_len_exp = max([len(os.path.basename(x)) for x in model_path]) + 2
 
-    print(f"|{'Model name':^{max_len_exp}}|{'Loss':^17}|{'Top1 err':^17}|{'Top1 acc':^17}|")
+    print(f"|{'Model name':^{max_len_exp}}|{'Loss':^{margin}}|{'Top1 err':^{margin}}|{'Top1 acc':^{margin}}|")
 
     except_list = []
     pl_mi = u"\u00B1"
 
     for path in model_path:
-        checkpoint, args = None, None
-
         pth_file = path + "/model_best.pth"
         conf_file = path + "/params.json"
 
@@ -52,9 +50,6 @@ def main():
         args.__dict__.update(params)
 
         checkpoint = torch.load(pth_file)
-
-        if checkpoint is None or args is None:
-            except_list.append(f"checkpoint and params are not exist in {path}")
 
         if args.model == 'resnet':
             model = ResNet(args.depth, 100, args.bottleneck)
@@ -74,24 +69,16 @@ def main():
         model.load_state_dict(checkpoint["model_state_dict"])
 
         # evaluate on validation set
-        err1_list, val_loss_list = test(val_loader, model, criterion, path)
+        err1, loss = test(val_loader, model, criterion, path)
 
-        err1, err1_moe = margin_of_error(err1_list)
-        # err5, err5_moe = margin_of_error(err5_list)
-        loss, loss_moe = margin_of_error(val_loss_list)
-
-        err1_string = f'{err1:.3f} {pl_mi} {err1_moe:.3f}'
-        # err5_string = f'{err5:.3f} {pl_mi} {err5_moe:.3f}'
-        loss_string = f'{loss:.3f} {pl_mi} {loss_moe:.3f}'
-
-        print(f"|{path:^{max_len_exp}}|{loss_string:^17}|{err1_string:^17}|{100 - err1}")
+        print(f"|{os.path.basename(path):^{max_len_exp}}|{loss:^{margin}.3f}|{err1:^{margin}}|{100 - err1:^{margin}.3f}|")
 
 
 @torch.no_grad()
 def test(val_loader, model, criterion, path):
-    losses = []
-    top1 = []
-    # top5 = []
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    # top5 = AverageMeter
 
     f = open(f'{path + "/res"}.txt', "w")
 
@@ -110,12 +97,12 @@ def test(val_loader, model, criterion, path):
         for pred in pred_list:
             f.write(str(pred.item()) + "\n")
 
-        losses.append(loss.item())
-        top1.append(err1.item())
+        losses.update(loss.item(), input.size(0))
+        top1.update(err1.item(), input.size(0))
         # top5.append(err5.item())
 
     f.close()
-    return top1, losses
+    return top1.avg, losses.avg
 
 
 if __name__ == "__main__":
